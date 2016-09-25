02ac3ddf6b1bc13635e852413178344002b23328
Rastislav Ondrasek <ras.ondrasek@gmail.com>
2016-09-25 16:04:54 -0700

    Reducing space complexity of the algorithm by using only 2 columns instead of N

--- hw3/knapsack.rb
+++ hw3/knapsack.rb
@@ -15,23 +15,23 @@ ARGF.each do |line|
 end
 
 # Initialize the 2D array
-a = Hash.new(0)
+a = Array.new(2) { Array.new(W + 1) }
 for x in 0..W
-  a[[0, x]] = 0
+  a[0][x] = 0
 end
 
 # Compute the largest value of items that fit into the knapsack
 for i in 1..n
   for x in 0..W
     # Case 1 when don't add current item to the knapsack
-    val1 = a[[i - 1, x]]
+    val1 = a[(i - 1) % 2][x]
     val2 = 0
     if (x - items[i][:weight] >= 0)
       # Case 2 when we are able to fit current item into knapsack of capacity x
-      val2 = a[[i - 1, x - items[i][:weight]]] + items[i][:value]
+      val2 = a[(i - 1) % 2][x - items[i][:weight]] + items[i][:value]
     end 
-    a[[i, x]] = [val1, val2].max
+    a[i % 2][x] = [val1, val2].max
   end
 end
 
-puts a[[n, W]]
+puts a[n % 2][W]
